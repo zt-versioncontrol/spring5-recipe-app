b76b971b76d4afa5cd5b71c66cd6f66fe72f2938
ztexdee <tap879@gmail.com>
2021-12-02 17:41:29 +0300

    added index page

---
+++ src/main/java/demo/springframework/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package demo.springframework.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"", "/", "/index"})
+public class IndexController {
+
+    public String getIndexPage(){
+        return "index";
+    }
+}
