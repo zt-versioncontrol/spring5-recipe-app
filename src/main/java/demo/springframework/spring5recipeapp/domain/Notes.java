66b025f7f647e1f31afe8ec2f60075e63f91cbb0
ztexdee <tap879@gmail.com>
2021-12-25 10:25:58 +0300

    Added Recipe and Notes entities

---
+++ src/main/java/demo/springframework/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,32 @@
+package demo.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
