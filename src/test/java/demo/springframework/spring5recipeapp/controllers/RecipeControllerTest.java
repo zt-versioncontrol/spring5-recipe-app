bba3ba8660b74cb9921000c143c05cc644a912cb
ztexdee <tap879@gmail.com>
2022-02-26 12:35:34 +0300

    Added tests for RecipeController

--- src/test/java/demo/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/demo/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -1,5 +1,6 @@
 package demo.springframework.spring5recipeapp.controllers;
 
+import demo.springframework.spring5recipeapp.commands.RecipeCommand;
 import demo.springframework.spring5recipeapp.domain.Recipe;
 import demo.springframework.spring5recipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -41,5 +43,42 @@ class RecipeControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/recipe/show/1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("recipe/show"));
+    }
+
+    @Test
+    public void testGetNewRecipeForm() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/new"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("recipe/form"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("recipe"));
+    }
+
+    @Test
+    public void testPostNewRecipeForm() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        Mockito.when(recipeService.saveRecipeCommand(Mockito.any())).thenReturn(command);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/recipe")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        )
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/recipe/show/2"));
+    }
+
+    @Test
+    public void testGetUpdateView() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        Mockito.when(recipeService.findCommandById(Mockito.anyLong())).thenReturn(command);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/update/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("recipe/form"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("recipe"));
     }
 }
